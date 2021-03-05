6d4ef5826b2bc5911ef2359e84e90d79da6736c8
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-05 13:15:00 -0500

    rud. plot added

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,27 @@
-// @TODO: YOUR CODE HERE!
+var raw_data= d3.csv('assets/data/data.csv').then(row => {
+    // transposes data --> an array per col
+    var tranposed_data = Object.assign(...Object.keys(row[0]).map(key =>
+        ({[key]: row.map( o => o[key])})
+    ));
+    
+    var trace = {
+        x: tranposed_data.age,
+        y: tranposed_data.income,
+        mode: 'markers',
+        type: 'scatter'
+      };
+    
+      // Create the data array for our plot
+      var data = [trace];
+    
+      // Define the plot layout
+      var layout = {
+        title: "Age v. Income",
+        xaxis: { title: "Age" },
+        yaxis: { title: "Income"}
+      };
+    
+      // Plot the chart to a div tag with id "bar-plot"
+      Plotly.newPlot("scatter", data, layout);
+});
+
