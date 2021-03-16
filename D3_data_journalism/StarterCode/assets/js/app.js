a9e4d328fdf4536e67d9c9ea751d694832449ba9
Ryan EM <ryan.eccleston.murdock@gmail.com>
2021-03-16 09:39:33 -0400

    version with pts and labels

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,58 +1,96 @@
-var width = 700;
-var height = 500;
+var svgWidth = 960;
+var svgHeight = 500;
 
-// var x = d3.scale.linear().range([0, width]);
-// var y = d3.scale.linear().range([height, 0]);
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 40
+};
 
-var raw_data= d3.csv('assets/data/data.csv').then(row => {
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
-    // transposes data --> an array per col
-    var tranposed_data = Object.assign(...Object.keys(row[0]).map(key =>
-        ({[key]: row.map( o => o[key])})
-    ));
-    
-    var trace = {
-        x: tranposed_data.poverty,
-        y: tranposed_data.healthcareLow,
-        mode: 'markers',
-        type: 'scatter'
-      };
-    
-      // Create the data array for our plot
-      var data = [trace];
-    
-      // Define the plot layout
-      var layout = {
-        xaxis: { title: "poverty" },
-        yaxis: { title: "low healthcare"}
-      };
-    
-      // Plot the chart to a div tag with id 'scatter'
-      Plotly.newPlot("scatter", data, layout);
 
-      // create g tag to be layer of for markers with state names 
-      // grabbng correct svg? created group?
-    
-      
-      var svg = d3.select('.main-svg').attr('height', height).attr('width', width);
+function castVals(tranposed_data) {
+  Object.keys(tranposed_data).forEach(key => {
+    // Checks if value is a string 
+    if (tranposed_data[key][0] % 10 > 0){
+      tranposed_data[key] = tranposed_data[key].map(p => parseFloat(p));
+    }
+    tranposed_data.income = tranposed_data.income.map(p => parseInt(p));
+  });
+  return tranposed_data;
+};
 
-      var dots =  svg.select('g').data(tranposed_data).enter().append('g').attr('class', 'marker-labels');
-      
-      dots.append("circle")
-      .attr("class", "dot")
-      .attr("r", '1')
-      .attr("cx", function (d) {
-          console.log(d);
-          return d.poverty;
-      })
-      .attr("cy", function (d) {
-          return d.healthcareLow;
-      })
-      .style("fill", 'blue');
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-      console.log(dots);
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-      console.log(tranposed_data);
+var raw_data = d3.csv('assets/data/data.csv').then(data => {
+  console.log(data);
+  data.forEach(d => {
+    d.poverty = +d.poverty,
+    d.healthcareLow = +d.healthcareLow;
+  });
+
+var xLinearScale = d3.scaleLinear()
+  .domain(d3.extent(data, d => d.poverty))
+  .range([0, width]);
+
+var yLinearScale = d3.scaleLinear()
+  .domain([0, d3.max(data, d => d.healthcareLow)])
+  .range([height, 0]);
+
+var xAxis = d3.axisBottom(xLinearScale);
+var yAxis = d3.axisLeft(yLinearScale);
+
+chartGroup.append("g")
+  .attr("transform", `translate(0, ${height})`)
+  .call(xAxis);
+
+chartGroup.append("g").call(yAxis);
+
+// points 
+var circles  = chartGroup.selectAll("circle")
+.data(data)
+.enter()
+.append("circle")
+.attr("r", d => 2.5 * (d.income / 11500))
+.attr("cx", d => xLinearScale(d.poverty))
+.attr("cy", d => yLinearScale(d.healthcareLow))
+.attr('value', d => d.abbr)
+.classed('pt', true);
+
+// point labels
+chartGroup.selectAll('text')
+.data(data).enter()
+.append("text")
+.attr("dx", d => xLinearScale(d.poverty) - 11)
+.attr('dy', d => yLinearScale(d.healthcareLow) + 6)
+.attr('value', d => d.abbr)
+.text(d => d.abbr)
+.classed('pt-label', true)
+
+// Axes
+chartGroup.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left + 40)
+.attr("x", 0 - (height / 2))
+.attr("dy", "1em")
+.attr("class", "axisText")
+.text('healthcare low');
+
+chartGroup.append("text")
+.attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+.attr("class", "axisText")
+.text("poverty");
+
+}).catch(function(error) {
+  console.log(error);
 });
-
-
